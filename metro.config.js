57b1e08cadbc2fa9b97fdbb5076b2bbe96c3c4e4
Agougil Mohamed <agougilmohamed03@gmail.com>
2025-07-17 21:40:23 +0100

    configure drizzle

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './constants/global.css' })
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts.push("sql");
+
+const finalConfig = withNativeWind(config, { input: "./constants/global.css" });
+
+module.exports = finalConfig;
